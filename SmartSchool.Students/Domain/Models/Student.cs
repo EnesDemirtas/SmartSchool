24e3098ace4a1ca86a99a13c463a16e66449ee2f
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-01-15 02:14:32 +0100

    feat: add domain models for Student service

---
+++ SmartSchool.Students/Domain/Models/Student.cs
@@ -0,0 +1,51 @@
+﻿namespace SmartSchool.Students.Domain.Models
+{
+    public class Student
+    {
+        #region Constructors and properties
+        public Student()
+        {
+            Relatives = new List<Relative>();
+        }
+        public int Id { get; set; }
+        public required string RollNumber { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Email { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public int Age => CalculateAge();
+        public string? PhoneNumber { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Relative> Relatives { get; set; }
+        #endregion
+
+        #region Public behavior
+        public static Student Create(string rollNumber, string firstName, string lastName, DateOnly dateOfBirth)
+        {
+            return new Student 
+            { 
+                RollNumber = rollNumber,
+                FirstName = firstName,
+                LastName = lastName,
+                DateOfBirth = dateOfBirth
+            };
+        }
+        #endregion
+
+        #region Private behavior
+        private int CalculateAge()
+        {
+            var currentDate = DateTime.UtcNow;
+            var age = currentDate.Year - DateOfBirth.Year;
+
+            if (currentDate.Month < DateOfBirth.Month || 
+                (currentDate.Month == DateOfBirth.Month && currentDate.Day < DateOfBirth.Day))
+            {
+                age--;
+            }
+
+            return age;
+        }
+        #endregion
+    }
+}
